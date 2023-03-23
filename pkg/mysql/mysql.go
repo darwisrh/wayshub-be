93276954179ba5631d30fab7666a8bf248341c48
Darwis <107021419+darwisrh@users.noreply.github.com>
2023-03-23 13:23:22 +0700

    result

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -12,11 +12,11 @@ var DB *gorm.DB
 // Connection Database
 func DatabaseInit() {
 	var err error
-	var DB_HOST = os.Getenv("DB_HOST")
-	var DB_USER = os.Getenv("DB_USER")
-	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	var DB_NAME = os.Getenv("DB_NAME")
-	var DB_PORT = os.Getenv("DB_PORT")
+	var DB_HOST = "containers-us-west-132.railway.app"
+	var DB_USER = "postgres"
+	var DB_PASSWORD = "17SwerQUGezUe1JymOAw"
+	var DB_NAME = "railway"
+	var DB_PORT = "5582"
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
